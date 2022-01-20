bb4e66e801775386cb0fa3168b18f84dc5a87e54
kkammoo <rlaansus89@gmail.com>
2022-01-20 09:29:11 +0900

    김무년작업

--- cafe/js/main/main.js
+++ cafe/js/main/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function imgSrc() {
+{
   // 경로 저장
-  const src1 = '../../cafeImg/썸네일1.png';
-  const src2 = '../../cafeImg/썸네일2.png';
-  const src3 = '../../cafeImg/썸네일3.png';
-  const src4 = '../../cafeImg/썸네일4.png';
+  const src1 = '../../cafeImg/cafe1/썸네일1.png';
+  const src2 = '../../cafeImg/cafe1/썸네일2.png';
+  const src3 = '../../cafeImg/cafe1/썸네일3.png';
+  const src4 = '../../cafeImg/cafe1/썸네일4.png';
 
   // 경로를 배열에 담기
   const cafeImgArray = [src1, src2, src3, src4];
@@ -14,7 +14,79 @@ function imgSrc() {
   let indexNum = Math.floor(Math.random() * 4);
   console.log(indexNum);
 
-  const $cafeThumb = document.querySelector('.cafeThumb');
+  const $cafeThumb = document.querySelector('.cafeThumb1');
+
+  $cafeThumb.src = cafeImgArray[indexNum];
+}
+{
+  // 경로 저장
+  const src1 = '../../cafeImg/cafe3/썸네일1.png';
+  const src2 = '../../cafeImg/cafe3/썸네일2.png';
+  const src3 = '../../cafeImg/cafe3/썸네일3.png';
+  const src4 = '../../cafeImg/cafe3/썸네일4.png';
+
+  // 경로를 배열에 담기
+  const cafeImgArray = [src1, src2, src3, src4];
+
+  // 인덱스 번호 랜덤 추출
+  let indexNum = Math.floor(Math.random() * 4);
+  console.log(indexNum);
+
+  const $cafeThumb = document.querySelector('.cafeThumb3');
+
+  $cafeThumb.src = cafeImgArray[indexNum];
+}
+{
+  // 경로 저장
+  const src1 = '../../cafeImg/cafe5/썸네일1.png';
+  const src2 = '../../cafeImg/cafe5/썸네일2.png';
+  const src3 = '../../cafeImg/cafe5/썸네일3.png';
+  const src4 = '../../cafeImg/cafe5/썸네일4.png';
+
+  // 경로를 배열에 담기
+  const cafeImgArray = [src1, src2, src3, src4];
+
+  // 인덱스 번호 랜덤 추출
+  let indexNum = Math.floor(Math.random() * 4);
+  console.log(indexNum);
+
+  const $cafeThumb = document.querySelector('.cafeThumb5');
+
+  $cafeThumb.src = cafeImgArray[indexNum];
+}
+{
+  // 경로 저장
+  const src1 = '../../cafeImg/cafe7/썸네일1.png';
+  const src2 = '../../cafeImg/cafe7/썸네일2.png';
+  const src3 = '../../cafeImg/cafe7/썸네일3.png';
+  const src4 = '../../cafeImg/cafe7/썸네일4.png';
+
+  // 경로를 배열에 담기
+  const cafeImgArray = [src1, src2, src3, src4];
+
+  // 인덱스 번호 랜덤 추출
+  let indexNum = Math.floor(Math.random() * 4);
+  console.log(indexNum);
+
+  const $cafeThumb = document.querySelector('.cafeThumb7');
+
+  $cafeThumb.src = cafeImgArray[indexNum];
+}
+{
+  // 경로 저장
+  const src1 = '../../cafeImg/cafe9/썸네일1.png';
+  const src2 = '../../cafeImg/cafe9/썸네일2.png';
+  const src3 = '../../cafeImg/cafe9/썸네일3.png';
+  const src4 = '../../cafeImg/cafe9/썸네일4.png';
+
+  // 경로를 배열에 담기
+  const cafeImgArray = [src1, src2, src3, src4];
+
+  // 인덱스 번호 랜덤 추출
+  let indexNum = Math.floor(Math.random() * 4);
+  console.log(indexNum);
+
+  const $cafeThumb = document.querySelector('.cafeThumb9');
 
   $cafeThumb.src = cafeImgArray[indexNum];
 }
