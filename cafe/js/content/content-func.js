9b7c5ab0bfe43f4b87f16ffb6e9cb7dd7e7b625f
Leegmmango <glanz9794@gmail.com>
2022-02-14 00:42:08 +0900

    수정

--- cafe/js/content/content-func.js
+++ cafe/js/content/content-func.js
@@ -12,21 +12,18 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
+  let i;
+  const $shopImg = document.getElementsByClassName("shop-img");
+
+  if (n > $shopImg.length) {
     slideIndex = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length;
+    slideIndex = $shopImg.length;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (i = 0; i < $shopImg.length; i++) {
+    $shopImg[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+
+  $shopImg[slideIndex - 1].style.display = "block";
 }
