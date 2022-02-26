cc37093c33d30049e703fbc3bc52af54544d559c
yeongbhinkim <abc282v@naver.com>
2022-02-26 14:56:53 +0900

    220226

--- cafe/js/home/home 3차수정.js
+++ cafe/js/home/home 3차수정.js
@@ -29,6 +29,18 @@ flagSeoul2 = false;
 let count = 0;
 
 const $hashtag = document.querySelector('.hashtags');
+const $inputtext = document.getElementById('inputtext');
+
+
+
+
+function backspace() { 
+  if($inputtext.value == ""){  
+	if (window.event.keyCode == 8) {
+    $hashtag.lastChild.querySelector('.reset').click();  
+    }
+  }
+}
 
 function click_f(evt) {
   
@@ -92,21 +104,74 @@ function newItemBtn(itemBtn1, itemBtn2, itemBtn3) {
   }
 }
 
-
-
-
-// 해시태그 초기화 버튼
-if (evt.target.matches('.resetBtn')) {
-  console.log('초기화 버튼');
-  document.getElementById('selectArea1').innerHTML = null;
- document.getElementById('selectArea2').innerHTML = null;
- document.getElementById('selectArea3').innerHTML = null;       
+//지역 체크박스
+if (evt.target.matches('.Ulsan100')) {
+  combox('.Ulsan100', Ulsan10);
+  console.log('울산중구');
+}
+if (evt.target.matches('.Ulsan110')) {
+  combox('.Ulsan110', Ulsan11);
+  console.log('울산북구');
+}
+if (evt.target.matches('.Ulsan120')) {
+  combox('.Ulsan120', Ulsan12);
+  console.log('울산동구');
+}
+function combox(item1,item2) {
+  const $item1 = document.querySelector(item1);
   
+  if (item2.checked == false) {    
+    $item1.style.color = "red";
+    console.log(item2.checked);
+    item2.checked == true;
+    
+  } else if (item2.checked == true) {    
+    $item1.style.color = "black";
+    console.log(item2.checked);    
+  }
+  console.log(item2.checked);
+}
+
+
+// if (evt.target.matches('.resetBtn')) {
+//   comboxBtn('.Ulsan100', '.Ulsan100');
+//   console.log('울산중구');
+// }
+// function comboxBtn(itemBtn1, itemBtn2) {
+//   console.log('체크체크지역2222222222');
+
+//   if (evt.target.matches(itemBtn1)) {
+//     console.log('체크해제지역33333333333');
+//     document.querySelector(itemBtn2).click();
+//   }  
+// }
+// 
+
+
+
+// 초기화 버튼
+if (evt.target.matches('.resetBtn')) {
+//   console.log('초기화 버튼');
+//   document.getElementById('selectArea1').innerHTML = null;
+//  document.getElementById('selectArea2').innerHTML = null;
+//  document.getElementById('selectArea3').innerHTML = null;    
+  
+comboxBtn('.Ulsan100',Ulsan10);
+comboxBtn('.Ulsan110',Ulsan11);
+comboxBtn('.Ulsan120',Ulsan12);
+function comboxBtn(itemBtn,itemBtn2) {
+  if (itemBtn2.checked == true) {  
+      document.querySelector(itemBtn).click();
+    }  
+  }  
   for(i=0; i<10; i++){
     document.querySelector('.reset').click();  
   }
-
 }
+
+
+
+
 
   //상세버튼 출력
   const $toMove = evt.target.closest('a');
@@ -132,30 +197,30 @@ if (evt.target.matches('.resetBtn')) {
     //   inputText.value = null;  
     //  }
   
-//지역 초기화
-if(evt.target.matches('.reset')){
-   document.getElementById('selectArea1').innerHTML = null;
-   document.getElementById('selectArea2').innerHTML = null;
-   document.getElementById('selectArea3').innerHTML = null;        
+// //지역 초기화
+// if(evt.target.matches('.reset')){
+//    document.getElementById('selectArea1').innerHTML = null;
+//    document.getElementById('selectArea2').innerHTML = null;
+//    document.getElementById('selectArea3').innerHTML = null;        
    
-}
+// }
   //지역 시 초기화
-  if(evt.target.matches('.btn')){
+  // if(evt.target.matches('.btn')){
   //  document.getElementById('selectArea1').innerHTML = null;
-   document.getElementById('selectArea2').innerHTML = null;
-   document.getElementById('selectArea3').innerHTML = null;        
+  //  document.getElementById('selectArea2').innerHTML = null;
+  //  document.getElementById('selectArea3').innerHTML = null;        
    
   //  document.getElementById('btn tab1').click();
-  }
-  if(evt.target.matches('.btn1')){
-     document.getElementById('selectArea3').innerHTML = null;             
-    }
-    if(evt.target.matches('.btn2')){
-      document.getElementById('selectArea3').innerHTML = null;             
-     }
-     if(evt.target.matches('.btn3')){
-      document.getElementById('selectArea3').innerHTML = null;             
-     }
+  // }
+  // if(evt.target.matches('.btn1')){
+  //    document.getElementById('selectArea3').innerHTML = null;             
+  //   }
+  //   if(evt.target.matches('.btn2')){
+  //     document.getElementById('selectArea3').innerHTML = null;             
+  //    }
+    //  if(evt.target.matches('.btn3')){
+    //   document.getElementById('selectArea3').innerHTML = null;             
+    //  }
      
 
  
@@ -164,18 +229,17 @@ if(evt.target.matches('.reset')){
   if (evt.target.matches('.btn')) {
     $toMove.addEventListener('click', (evt) => {
       
-       document.getElementById('selectArea1').innerHTML = $toMove.textContent;
+      //  document.getElementById('selectArea1').innerHTML = $toMove.textContent;
       //  document.getElementById('btn tab2').click();
     });
     if (evt.target.matches('.Ulsan') && !flagUlsan) {
-
+     
       const $Ele = makeElement('ul', { 'class': 'com1 list3', 'id': '' },
-        makeElement('li', { 'class': 'com6 is_on3' },
-          makeElement('a', { 'href': '#tab13', 'class': 'com3 btn3 Ulsan2' }, "울산중구")),
         makeElement('li', {},
-          makeElement('a', { 'href': '#tab23', 'class': 'com3 btn3 Ulsan2' }, "울산북구")),
+        makeElement('label', {'class': 'com3 btn3 Ulsan2 Ulsan100', 'id' : 'Ulsan100'},"울산중구", makeElement('input', { 'type': 'checkbox', 'id' : 'Ulsan10'}))),
         makeElement('li', {},
-          makeElement('a', { 'href': '#tab33', 'class': 'com3 btn3 Ulsan2' }, "울산동구"))
+        makeElement('label', {'class': 'com3 btn3 Ulsan2 Ulsan110', 'id' : 'Ulsan110'},"울산북구", makeElement('input', { 'type': 'checkbox', 'id' : 'Ulsan11'}))),makeElement('li', {},
+        makeElement('label', {'class': 'com3 btn3 Ulsan2 Ulsan120', 'id' : 'Ulsan120'},"울산동구", makeElement('input', { 'type': 'checkbox', 'id' : 'Ulsan12'})))
       );
       $tab3box.appendChild($Ele);
       flagUlsan = true;
@@ -183,7 +247,7 @@ if(evt.target.matches('.reset')){
     if (evt.target.matches('.Busan') && !flagBusan) {
 
       const $Ele = makeElement('ul', { 'class': 'com1 list2', 'id': '' },
-        makeElement('li', { 'class': 'com6 is_on2' },
+        makeElement('li', {},
           makeElement('a', { 'href': '#tab12', 'class': 'com3 btn2 Busan2' }, "부산중구")),
         makeElement('li', {},
           makeElement('a', { 'href': '#tab22', 'class': 'com3 btn2 Busan2' }, "부산북구")),
@@ -196,7 +260,7 @@ if(evt.target.matches('.reset')){
     if (evt.target.matches('.Seoul') && !flagSeoul) {
 
       const $Ele = makeElement('ul', { 'class': 'com1 list1', 'id': '' },
-        makeElement('li', { 'class': 'com6 is_on1' },
+        makeElement('li', {},
           makeElement('a', { 'href': '#tab11', 'class': 'com3 btn1 Seoul1' }, "서울중구")),
         makeElement('li', {},
           makeElement('a', { 'href': '#tab21', 'class': 'com3 btn1 Seoul1' }, "서울북구")),
@@ -208,69 +272,69 @@ if(evt.target.matches('.reset')){
     }
   }
   
-  //tabs2 2번
-  if (evt.target.matches('.btn3')) {
-    $toMove.addEventListener('click', (evt) => {
-        document.getElementById('selectArea2').innerHTML = $toMove.textContent;
+  // //tabs2 2번
+  // if (evt.target.matches('.btn3')) {
+  //   $toMove.addEventListener('click', (evt) => {
+  //       document.getElementById('selectArea2').innerHTML = $toMove.textContent;
 
-      //  document.getElementById('btn tab3').click();
-    });
-    if (evt.target.matches('.Ulsan2') && !flagUlsan2) {
+  // //     //  document.getElementById('btn tab3').click();
+  //   });
+  //   if (evt.target.matches('.Ulsan2') && !flagUlsan2) {
 
-      const $Ele3 = makeElement('div', { 'class': 'com4 area3' },
+  //     const $Ele3 = makeElement('div', { 'class': 'com4 area3' },
      
-        makeElement('a', { 'href': '#', 'id': 'tab13', 'class': 'com3 com2 cont3 test' }, '울산중구3'),
-        makeElement('a', { 'href': '#', 'id': 'tab23', 'class': 'com3 com2 cont3 test' }, '울산북구3'),
-        makeElement('a', { 'href': '#', 'id': 'tab33', 'class': 'com3 com2 cont3 test' }, '울산동구3')
-      ); 
-      $cont_area3.appendChild($Ele3); 
-      flagUlsan2 = true; 
-    } 
-  } 
+  //       makeElement('a', { 'href': '#', 'id': 'tab13', 'class': 'com3 com2 cont3 test' }, '울산중구3'),
+  //       makeElement('a', { 'href': '#', 'id': 'tab23', 'class': 'com3 com2 cont3 test' }, '울산북구3'),
+  //       makeElement('a', { 'href': '#', 'id': 'tab33', 'class': 'com3 com2 cont3 test' }, '울산동구3')
+  //     ); 
+  //     $cont_area3.appendChild($Ele3); 
+  //     flagUlsan2 = true; 
+    // } 
+  // } 
  
-  if (evt.target.matches('.btn2')) { 
-    $toMove.addEventListener('click', (evt) => { 
-       document.getElementById('selectArea2').innerHTML = $toMove.textContent; 
-      //  document.getElementById('btn tab3').click(); 
-    }); 
-    if (evt.target.matches('.Busan2') && !flagBusan2) { 
+  // if (evt.target.matches('.btn2')) { 
+  //   $toMove.addEventListener('click', (evt) => { 
+  //      document.getElementById('selectArea2').innerHTML = $toMove.textContent; 
+  //     //  document.getElementById('btn tab3').click(); 
+  //   }); 
+  //   if (evt.target.matches('.Busan2') && !flagBusan2) { 
  
-      const $Ele2 = makeElement('div', { 'class': 'com4 area2' }, 
+  //     const $Ele2 = makeElement('div', { 'class': 'com4 area2' }, 
        
-        makeElement('a', { 'href': '#', 'id': 'tab12', 'class': 'com3 com2 cont2 test' }, '부산중구2'),
-        makeElement('a', { 'href': '#', 'id': 'tab22', 'class': 'com3 com2 cont2 test' }, '부산북구2'),
-        makeElement('a', { 'href': '#', 'id': 'tab32', 'class': 'com3 com2 cont2 test' }, '부산동구2')
-      ); 
-      $cont_area2.appendChild($Ele2); 
-      flagBusan2 = true; 
-    } 
-  } 
+  //       makeElement('a', { 'href': '#', 'id': 'tab12', 'class': 'com3 com2 cont2 test' }, '부산중구2'),
+  //       makeElement('a', { 'href': '#', 'id': 'tab22', 'class': 'com3 com2 cont2 test' }, '부산북구2'),
+  //       makeElement('a', { 'href': '#', 'id': 'tab32', 'class': 'com3 com2 cont2 test' }, '부산동구2')
+  //     ); 
+  //     $cont_area2.appendChild($Ele2); 
+  //     flagBusan2 = true; 
+    // } 
+  // } 
  
  
-  if (evt.target.matches('.btn1')) { 
-    $toMove.addEventListener('click', (evt) => { 
-       document.getElementById('selectArea2').innerHTML = $toMove.textContent; 
-      //  document.getElementById('btn tab3').click(); 
-    }); 
-    if (evt.target.matches('.Seoul1') && !flagSeoul2) { 
-      const $Ele2 = makeElement('div', { 'class': 'com4 area1' }, 
+  // if (evt.target.matches('.btn1')) { 
+  //   $toMove.addEventListener('click', (evt) => { 
+  //      document.getElementById('selectArea2').innerHTML = $toMove.textContent; 
+  //     //  document.getElementById('btn tab3').click(); 
+  //   }); 
+  //   if (evt.target.matches('.Seoul1') && !flagSeoul2) { 
+  //     const $Ele2 = makeElement('div', { 'class': 'com4 area1' }, 
      
-        makeElement('a', { 'href': '#', 'id': 'tab11', 'class': 'com3 com2 cont1 test' }, '서울중구2'),
-        makeElement('a', { 'href': '#', 'id': 'tab21', 'class': 'com3 com2 cont1 test' }, '서울북구2'),
-        makeElement('a', { 'href': '#', 'id': 'tab31', 'class': 'com3 com2 cont1 test' }, '서울동구2')
-      );
-      $cont_area1.appendChild($Ele2);
-      flagSeoul2 = true;
-    }
+  //       makeElement('a', { 'href': '#', 'id': 'tab11', 'class': 'com3 com2 cont1 test' }, '서울중구2'),
+  //       makeElement('a', { 'href': '#', 'id': 'tab21', 'class': 'com3 com2 cont1 test' }, '서울북구2'),
+  //       makeElement('a', { 'href': '#', 'id': 'tab31', 'class': 'com3 com2 cont1 test' }, '서울동구2')
+  //     );
+  //     $cont_area1.appendChild($Ele2);
+  //     flagSeoul2 = true;
+    // }
 
 
-  }
-  if (evt.target.matches('.test')) {
-    $toMove.addEventListener('click', (evt) => {
-       document.getElementById('selectArea3').innerHTML = $toMove.textContent;
-      //  document.getElementById('btn tab3').click();
-    });  
-  }  
+  // }
+  // if (evt.target.matches('.test')) {
+  //   $toMove.addEventListener('click', (evt) => {
+  //      document.getElementById('selectArea3').innerHTML = $toMove.textContent;
+  //     //  document.getElementById('btn tab3').click();
+  //   });  
+  // }  
 
 
   /**
@@ -278,12 +342,12 @@ if(evt.target.matches('.reset')){
    * **/
   // 시
   newFunction('.list li', '.cont', '.btn', 'com6');
-  //서울
-  newFunction('.list1 li', '.cont1', '.btn1', 'com6');
-  //부산
-  newFunction('.list2 li', '.cont2', '.btn2', 'com6');
-  //울산
-  newFunction('.list3 li', '.cont3', '.btn3', 'com6');
+  // 서울
+  // newFunction('.list1 li', '.cont1', '.btn1', 'com6');
+  // //부산
+  // newFunction('.list2 li', '.cont2', '.btn2', 'com6');
+  // //울산
+  // newFunction('.list3 li', '.cont3', '.btn3', 'com6');
 
   function newFunction(attr1, attr2, attr3, attr4) {
 
