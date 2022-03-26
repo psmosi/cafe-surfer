c4eb2a848dcdebc5f80770a2d05a6ef7983fd44a
kimYeongBhin <abc282v@naver.com>
2022-03-26 12:25:34 +0900

    북마크

--- 합본/cafesurfer/src/main/java/com/kh/cafesurfer/domain/bookMark/BookMark.java
+++ 합본/cafesurfer/src/main/java/com/kh/cafesurfer/domain/bookMark/BookMark.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 @Data
 public class BookMark {
 
-private Long bookmarkChecked;             //  BOOKMARK_CHECKED	NUMBER
 private LocalDateTime bookmarkChkdate;    //  BOOKMARK_CHKDATE	TIMESTAMP(6)
 private Long memberId;                    //  MEMBER_ID	NUMBER(5,0)
 private Long shopId;                      //  SHOP_ID	NUMBER(5,0)
