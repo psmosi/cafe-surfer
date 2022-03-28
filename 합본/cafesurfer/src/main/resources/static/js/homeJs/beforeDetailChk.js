84db6a3f1d24175cb4467536874b75ffa6bb20b2
kimYeongBhin <abc282v@naver.com>
2022-03-28 19:25:53 +0900

    페이지

---
+++ 합본/cafesurfer/src/main/resources/static/js/homeJs/beforeDetailChk.js
@@ -0,0 +1,21 @@
+const $body = document.querySelector('.header_top_wrap');
+$body?.addEventListener('click', click_f, true);
+
+const $detail_icon = document.querySelector('.detail-icon');
+const $detailCHK = document.querySelector('.detailCHK');
+
+function click_f(evt) {
+//상세버튼 출력
+if (evt.target.matches('.detail-icon')) {
+
+ if ($detailCHK.checked == true) {
+   $detail_icon.addEventListener('click', (evt) => {
+    $detailCHK.checked = false;
+   });
+ } else if ($detailCHK.checked == false) {
+   $detail_icon.addEventListener('click', (evt) => {
+    $detailCHK.checked = true;
+   });
+ }
+} 
+}
